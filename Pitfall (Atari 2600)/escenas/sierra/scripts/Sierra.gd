8f940933cde3522a427a6e12204e0991d6ed9ef5
DigitalToyz <ask.digitaltoyz@gmail.com>
2022-04-18 20:29:23 +0200

    Pitfall (Atari 2600)
    + Añadido juice a la sierra

--- Pitfall (Atari 2600)/escenas/sierra/scripts/Sierra.gd
+++ Pitfall (Atari 2600)/escenas/sierra/scripts/Sierra.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal vibracion
+
 export(bool) var estatica
 
 const VELOCIDAD_HORIZONTAL:float = -25.0
@@ -7,8 +9,10 @@ const VELOCIDAD_HORIZONTAL:float = -25.0
 func _ready():
 	if not estatica:
 		$AnimatedSprite.play("encendida")
+		$CPUParticles2D.emitting = true
 	else:
 		$AnimatedSprite.play("apagada")
+		$CPUParticles2D.emitting = false
 
 func _physics_process(delta):
 	if not estatica:
@@ -18,3 +22,6 @@ func _physics_process(delta):
 func reiniciar_posicion() -> void:
 	if position.x < 0:
 		position.x = get_viewport_rect().size.x
+
+func _on_Timer_timeout():
+	emit_signal("vibracion")
