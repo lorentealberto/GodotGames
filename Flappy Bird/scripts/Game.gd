abccd3bf553341fcca1aad6c864c00825b8656a7
Alberto Lorente <lorentealberto@yahoo.es>
2022-10-25 17:14:35 +0200

    Rompe Ralph: Escenario

--- Flappy Bird/scripts/Game.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-func _on_DeadArea_body_entered(body):
-	if body.name == "Player":
-		get_tree().reload_current_scene()
--- Flappy Bird/scripts/Pipe.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody2D
-
-
-func _physics_process(delta):
-	position.x -= 100 * delta
-
-
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
