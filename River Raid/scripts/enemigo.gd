22bb1add22178691cb76c6d08a244c3537cf0a5c
DigitalToyz <ask.digitaltoyz@gmail.com>
2022-11-02 15:23:13 +0100

    River Raid

--- River Raid/scripts/enemigo.gd
+++ River Raid/scripts/enemigo.gd
@@ -2,11 +2,26 @@ extends Area2D
 class_name Enemigo
 
 
+var activado: bool = false
+
+
+func _ready() -> void:
+	randomize()
+	$AnimatedSprite.flip_h = randi() % 2 == 0
+
+
+func _process(delta: float) -> void:
+	if not activado:
+		if (get_node("../../Avion") as KinematicBody2D).global_position.distance_to(global_position) < 20:
+			activado = true
+	else:
+		pass
+
 func destruir() -> void:
 	$AnimatedSprite.visible = false
 	$Sprite.visible = true
 	$Timer.start()
 
 
-func _on_Timer_timeout():
+func _on_Timer_timeout() -> void:
 	queue_free()
--- River Raid/scripts/gestor_enemigos.gd
+++ River Raid/scripts/gestor_enemigos.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
 export var helicoptero: PackedScene
+export var caza: PackedScene
+export var barco: PackedScene
 
 func _ready() -> void:
 	randomize()
-	var instancia_enemigo: Area2D = helicoptero.instance()
+	var instancia_enemigo: Area2D
+	match randi() % 3:
+		0:
+			instancia_enemigo = helicoptero.instance()
+		1:
+			instancia_enemigo = caza.instance()
+		2:
+			instancia_enemigo = barco.instance()
 	add_child(instancia_enemigo)
-	instancia_enemigo.position.x = floor(rand_range(0, 160))
+	instancia_enemigo.position.x = floor(rand_range($"../Fondo/Position2D".position.x, $"../Fondo/Position2D2".position.x))
